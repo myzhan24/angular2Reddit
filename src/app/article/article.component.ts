7215d91e56cb4cde18f41c4d618297dd8806c89d
Matthew Zhan <matthew.zhan@polycom.com>
2017-05-02 11:29:14 -0500

    Added app article section

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
 export class ArticleComponent implements OnInit {
+  private votes: number;
+  private title: string;
+  private link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
 
   ngOnInit() {
+  }
+
+  private voteUp(): void {
+    this.votes++;
+  }
+
+  private voteDown(): void {
+    this.votes--;
   }
 
 }
